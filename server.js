e8a8b2a6e0a5eead8f4e91e0ac3d7614c6e57d15
Robert Harężlak <harezlak.robert7@gmail.com>
2017-04-03 22:52:03 +0200

    simply json sender

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express')
 const faker = require('faker')
 const app = express()
+const port = 8080;
+const User = {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    website: faker.internet.url(),
+    address: faker.address.streetAddress(),
+    bio: faker.lorem.sentences()
+}
+
+app.get('/', (req, res) => {
+    res.json(User)
+})
+
+app.listen(port, () => {
+    console.log(`Server listen on ${port}`)
+})
